de5754ee30148ff1e46cdabe131d866c44fae518
Andre Furlan <ensismoebius@gmail.com>
2020-06-23 14:27:46 -0300

    Melhorei os comentários
    Adicionei comentários ao loop que repete a mensagem
    de boas vindas

--- principal.cpp
+++ principal.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 // Início do programa
 int main(int arcgc, char** argv){
+
+	// Repete a mensagem de boas vindas
 	for(int i=0; i<100; i++){
 		std::cout << "Programa iniciado" << std::endl;
 	}
